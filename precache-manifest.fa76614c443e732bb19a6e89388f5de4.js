ce77b6f0deec830599b6c3dcb21e9e11d63368d0
danillotc <danillotc@hotmail.com>
2020-07-02 18:47:07 -0300

    Updates

---
+++ precache-manifest.fa76614c443e732bb19a6e89388f5de4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ea3e38c106b41c95bc58793f8e4f582",
+    "url": "/conversor_moedas/index.html"
+  },
+  {
+    "revision": "f434f354a7399e209743",
+    "url": "/conversor_moedas/static/css/main.c8ee8b5b.chunk.css"
+  },
+  {
+    "revision": "641273a404d3d962d50e",
+    "url": "/conversor_moedas/static/js/2.0b92f01e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/conversor_moedas/static/js/2.0b92f01e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f434f354a7399e209743",
+    "url": "/conversor_moedas/static/js/main.5eb84f6e.chunk.js"
+  },
+  {
+    "revision": "f6a34a36087c08ac5e31",
+    "url": "/conversor_moedas/static/js/runtime-main.aee6376c.js"
+  }
+]);
